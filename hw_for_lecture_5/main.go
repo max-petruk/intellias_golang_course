2f80c4fdaa3773e3c5054a2573679e83ba4f8a96
Max Petruk <dev.maxpetruk@gmail.com>
2022-06-16 12:05:45 +0300

    Add 2 new variables applesToBuy and pearsToBuy instead of just digits in the formulas, we may have to buy a different number of the fruits later

--- hw_for_lecture_5/main.go
+++ hw_for_lecture_5/main.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	var currency string = "грн"
 	var pieces string = "шт."
+	var applesToBuy = 9
+	var pearsToBuy = 8
 	const applePrice = 5.99
 	const pearPrice = 7.00
 
@@ -15,7 +17,7 @@ func main() {
 
 	fmt.Println("Одне яблуко коштує", applePrice, currency)
 	fmt.Println("Одна груша коштує", pearPrice, currency)
-	fmt.Println("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?",  math.Round(float64(applePrice*9+float32(pearPrice)*8)), currency)
+	fmt.Println("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?",  applePrice*float64(applesToBuy)+pearPrice*float64(pearsToBuy), currency)
 	fmt.Println("2. Скільки груш ми можемо купити?", math.Floor(balance/pearPrice), pieces)
 	fmt.Println("3. Скільки яблук ми можемо купити?", float32(balance)/applePrice, pieces)
 	fmt.Println("4. Чи ми можемо купити 2 груші та 2 яблука?", "Так")
