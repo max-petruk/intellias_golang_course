eb4dd79f354e6caf65cc1147a9843bec094f10f6
Max Petruk <dev.maxpetruk@gmail.com>
2022-06-16 12:15:38 +0300

    modify the logic of the 4th question '4. Чи ми можемо купити 2 груші та 2 яблука?'

--- hw_for_lecture_5/main.go
+++ hw_for_lecture_5/main.go
@@ -20,5 +20,7 @@ func main() {
 	fmt.Println("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?",  applePrice*float64(applesToBuy)+pearPrice*float64(pearsToBuy), currency)
 	fmt.Println("2. Скільки груш ми можемо купити?", math.Floor(balance/pearPrice), pieces)
 	fmt.Println("3. Скільки яблук ми можемо купити?", math.Floor(balance/applePrice), pieces)
-	fmt.Println("4. Чи ми можемо купити 2 груші та 2 яблука?", "I wish I could")
+	applesToBuy = 2
+	pearsToBuy = 2
+	fmt.Println("4. Чи ми можемо купити 2 груші та 2 яблука?", applePrice*float64(applesToBuy)+pearPrice*float64(pearsToBuy) <= balance)
 }
