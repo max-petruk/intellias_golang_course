1066a8ca51f4ae8b8370f4a13c6fef4dfe96afb1
Max Petruk <dev.maxpetruk@gmail.com>
2022-06-22 18:12:33 +0300

    Replace some fmt.Println to fmt.PrintF and therefore get rid of 2 extra variables 'currency' and 'pieces'

--- hw_for_lecture_5/main.go
+++ hw_for_lecture_5/main.go
@@ -14,17 +14,26 @@ func main() {
 	)
 
 	const (
-		currency string = "грн"
-		pieces string = "шт."
+		// currency string = "грн"
+		// pieces string = "шт."
 		applePrice = 5.99
 		pearPrice = 7.00
 	)
 
-	fmt.Println("Одне яблуко коштує", applePrice, currency)
-	fmt.Println("Одна груша коштує", pearPrice, currency)
-	fmt.Println("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?",  applePrice*float64(applesToBuy)+pearPrice*float64(pearsToBuy), currency)
-	fmt.Println("2. Скільки груш ми можемо купити?", math.Floor(balance/pearPrice), pieces)
-	fmt.Println("3. Скільки яблук ми можемо купити?", math.Floor(balance/applePrice), pieces)
+	// fmt.Println("Одне яблуко коштує", applePrice, currency)
+	fmt.Printf("Одне яблуко коштує - %v грн.\n", applePrice)
+	// fmt.Println("Одна груша коштує", pearPrice, currency)
+	fmt.Printf("Одна груша коштує - %v грн.\n", pearPrice)
+
+	// fmt.Println("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?",  applePrice*float64(applesToBuy)+pearPrice*float64(pearsToBuy), currency)
+	fmt.Printf("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш? - %v грн.\n",  applePrice*float64(applesToBuy)+pearPrice*float64(pearsToBuy))
+
+
+	
+	// fmt.Println("2. Скільки груш ми можемо купити?", math.Floor(balance/pearPrice), pieces)
+	fmt.Printf("2. Скільки груш ми можемо купити? - %v шт.\n", math.Floor(balance/pearPrice))
+	// fmt.Println("3. Скільки яблук ми можемо купити?", math.Floor(balance/applePrice), pieces)
+	fmt.Printf("3. Скільки яблук ми можемо купити? -%v шт.\n", math.Floor(balance/applePrice))
 	applesToBuy = 2
 	pearsToBuy = 2
 
